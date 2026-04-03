757fa8fdf888ff8d889714fd56323b1fdc378834
brunooka <brunohideki31@gmail.com>
2026-04-03 19:03:57 -0300

    arrumando header cardapio e removendo js de avaliacao

--- src/js/avaliacao.js
+++ src/js/avaliacao.js
@@ -1,28 +1,3 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-
-  const container = document.querySelector('.depoimentos');
-
-  const cards = container.querySelector('.cards');
-  const next = container.querySelector('.next');
-  const prev = container.querySelector('.prev');
-  const total = container.querySelectorAll('.card').length;
-
-  let index = 0;
-
-  next.addEventListener('click', () => {
-    index = (index + 1) % total;
-    cards.style.transform = 'translateX(-${index * 100}%)';
-  });
-
-  prev.addEventListener('click', () => {
-    index = (index - 1 + total) % total;
-    cards.style.transform = 'translateX(-${index * 100}%)';
-  });
-
-});
-
-
 const menuToggle = document.getElementById('menu-toggle');
 const navBar = document.getElementById('nav-bar');
 
